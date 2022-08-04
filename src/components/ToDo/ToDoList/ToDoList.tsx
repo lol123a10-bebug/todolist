ae4fc1f24b56dd134ab1f7aef7db78726806ea11
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2022-08-04 16:02:47 +0500

    .

--- src/components/ToDo/ToDoList/ToDoList.tsx
+++ src/components/ToDo/ToDoList/ToDoList.tsx
@@ -3,34 +3,8 @@ import styled from "styled-components";
 import { servicesApi } from "../../../services";
 import Loading from "../../UI/Loading/Loading";
 
-const List = styled.ul`
-  display: grid;
-  justify-content: center;
-  grid-template-columns: 1fr;
-  padding-top: 5rem;
-  grid-gap: 2rem;
-
-  @media (min-width: 60rem) {
-    grid-template-columns: repeat(
-      ${({ length }) => (length < 2 ? length : 2)},
-      1fr
-    );
-  }
-
-  @media (min-width: 90rem) {
-    grid-template-columns: repeat(
-      ${({ length }) => (length < 3 ? length : 3)},
-      1fr
-    );
-  }
-`;
-
 const ToDoList = (props) => {
-  const {
-    data: todoList = [],
-    isLoading,
-    error,
-  } = servicesApi.useGetTodosQuery("");
+  const { data: todoList = [], isLoading, error } = servicesApi.useGetTodosQuery("");
 
   if (isLoading) return <Loading />;
 
@@ -39,16 +13,26 @@ const ToDoList = (props) => {
   return (
     <List length={todoList.length}>
       {todoList.map((item: any) => (
-        <ToDoItem
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          description={item.description}
-          done={item.done}
-        />
+        <ToDoItem key={item.id} id={item.id} title={item.title} description={item.description} done={item.done} />
       ))}
     </List>
   );
 };
 
 export default ToDoList;
+
+const List = styled.ul`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 1fr;
+  padding-top: 5rem;
+  grid-gap: 2rem;
+
+  @media (min-width: 60rem) {
+    grid-template-columns: repeat(${({ length }) => (length < 2 ? length : 2)}, 1fr);
+  }
+
+  @media (min-width: 90rem) {
+    grid-template-columns: repeat(${({ length }) => (length < 3 ? length : 3)}, 1fr);
+  }
+`;
